fc854484d43bd7b21ac5b81fa38b368c8b4d974d
airyox <114385799+airyox@users.noreply.github.com>
2022-11-09 16:54:37 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,18 @@
+arr = [1, 2, 3, 4, 5];
+function countOfNums(arr) {
+let count = 0;
+for (let i = 0; i < arr.length; i++) 
+{
+if (arr[i] % 2 !== 0) 
+{
+count++;
+}
+if (arr[i] % 2 != 0)
+
+{
+count++;
+}
+return console.log(count)
+}
+}
+countOfNums(arr);
---
+++ 2.js
@@ -0,0 +1,28 @@
+function isPrime(num) {
+    let _true = 'число простое', _false = 'ответ неверный';
+    if (num <= 1 && num > 1000) return _false;
+    if (num == 2) return _true;
+    for (let i = 2; i <= num / 2; i++) {
+    if (num % i == 0) {
+      return _false;
+    }
+    }
+    return _true;
+    }
+console.log(isPrime(1001));
+console.log(isPrime(53));
+
+
+
+ function isPrime(n) {
+            if (!Number.isInteger(n) || n < 2 || n > 1000) return "данные неверны";
+ 
+            let k = Math.sqrt(n);
+            for (let i = 2; i <= k; i++)
+                if (n % i === 0) return "не простое число";
+ 
+            return "простое число";
+        }
+ 
+        console.log(isPrime(555));
+        console.log(isPrime(7));
